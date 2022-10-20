852bd8cfcf8f0262085953944a72505162593ed7
Karyna-Borysenko <k.a.borysenko@gmail.com>
2022-10-20 14:32:17 +0300

    hw-03-phonebook -done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,14 +10,25 @@ import { Container, Header, Notice } from './App.styled';
 
 class App extends React.Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [{ id: 'id-1', name: '⭐ GoIT', number: '067 326 95 92' }],
     filter: '',
   };
+
+  //----Монтирование----
+  componentDidMount() {
+    const parsedLSContacts = JSON.parse(localStorage.getItem('contacts'));
+
+    if (parsedLSContacts) {
+      this.setState({ contacts: parsedLSContacts });
+    }
+  }
+
+  //----Обновление----
+  componentDidUpdate(prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   //----Добавляем контакты----
   addContact = ({ name, number }) => {
